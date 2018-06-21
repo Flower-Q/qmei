be1beb801c1906b342be31a205e609eca88e88e0
mei-iirose <40450955+mei-iirose@users.noreply.github.com>
2018-06-21 17:18:56 -0500

    Update qbot.js

--- qbot.js
+++ qbot.js
@@ -31,45 +31,54 @@
             default_reply: 'mei will bite you!'
         }),
         
-        MatchRule({
+        //this is working, media player provided by site
+            MatchRule({
             include_keyword: 'meis music',
             default_reply: '\http://touhouradio.com:8000/.mp3'
         }),
       
-        MatchRule({
+            MatchRule({
             include_keyword: 'bye',
             default_reply: 'bye!'
         }),
-            
+        
+        //fetches weather info for minnesota from noaa
             MatchRule({
             include_keyword: 'q!weather',
             default_reply: 'hazy with a chance of updates'
         }),
             
+        //pairs two users in the chat
             MatchRule({
             include_keyword: 'q!ship',
             default_reply: 'mei hasnt gotten a marriage license yet D:'
         }),
-            
+        
+        //displays random whole number between 0001 and 9999
             MatchRule({
             include_keyword: 'q!roll',
             default_reply: 'coming soon ^_^'
         }),
-                       
+        
+        //fetches horoscope
             MatchRule({
-            include_keyword: 'q!fortune',
+            include_keyword: 'q!fortune <zodiac>',
             default_reply: 'zodiac not yet available >~<'
         }),
-                       
+        
+        //initiates macro to give thief 4 gold
             MatchRule({
             include_keyword: '*steals gold*',
             default_reply: 'wha?! mei had more than this a moment ago...'
         }),
+            
+        //just for lulz    
             MatchRule({
             include_keyword: 'q!pantsu',
             default_reply: 'https://i.imgur.com/PAxxtIc.png'
         }),
-            
+        
+        //displays this source page
             MatchRule({
             include_keyword: 'show your brain',
             default_reply: '\https://raw.githubusercontent.com/mei-iirose/qbot/master/qbot.js'
