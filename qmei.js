052dcc1a5b1ce862648f183df744bfbd2479b288
鳴 「Mei」 <mei.iirose@pm.me>
2018-07-10 01:54:09 -0500

    qmei.js

--- qmei.js
+++ qmei.js
@@ -61,6 +61,31 @@
         }),
         MatchRule({
             begin_with_keyword: [myself + ' &lt;', 'q!'],
+            include_keyword: ['magic 8', 'magic8', 'Magic 8'],
+            default_reply:
+            ['● It is certain.',
+             '● It is decidedly so.',
+             '● Without a doubt.',
+             '● Yes - definitely.',
+             '● You may rely on it.',
+             '● As I see it, yes.',
+             '● Most likely.',
+             '● Outlook good.',
+             '● Yes.',
+             '● Signs point to yes.',
+             '● Reply hazy, try again.',
+             '● Ask again later.',
+             '● Better not tell you now.',
+             '● Cannot predict now.',
+             '● Concentrate and ask again.',
+             '● Dont count on it.',
+             '● My reply is no.',
+             '● My sources say no',
+             '● Outlook not so good.',
+             '● Very doubtful.']
+        }),
+        MatchRule({
+            begin_with_keyword: [myself + ' &lt;', 'q!'],
             include_keyword: ['Nier', 'nier'],
             default_reply: function () {var urls =
                 ['https://www.youtube.com/watch?v=xRHsEWIC8PI',
